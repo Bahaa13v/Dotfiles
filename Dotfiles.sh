acf9bacca5526122e05b9c6cbde44e14866142e9
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-07 23:35:51 +0100

    Update Dotfiles.sh

--- Dotfiles.sh
+++ Dotfiles.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
-mv packages.txt /home/bahaa
+sudo pacman -S --needed waybar alacritty sway wofi neofetch
+mv packages.txt /home/bahaa/
 mkdir /home/bahaa/.config
-cp -r * /home/bahaa/.config/
+cp -r alacritty /home/bahaa/.config/
+cp -r sway /home/bahaa/.config/
+cp -r neofetch /home/bahaa/.config/
+cp -r waybar /home/bahaa/.config/
+cp -r wofi /home/bahaa/.config/
