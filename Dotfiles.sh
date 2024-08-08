d1f668bbeb4f8f99ea81dc0533cf8d16e7e1e50a
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-08 15:14:32 +0100

    Update Dotfiles.sh

--- Dotfiles.sh
+++ Dotfiles.sh
@@ -14,9 +14,15 @@ cp -r wofi /home/bahaa/.config/
 
 cp -r walls /home/bahaa/
 
+
+echo "Setting up Zsh..."
 sudo pacman -S --needed --noconfirm --disable-download-timeout bash-preexec grml-zsh-config powerline python-click-completion zsh zsh-autosuggestions zsh-completions zsh-doc zsh-history-substring-search zsh-lovers zsh-syntax-highlighting zshdb
 
 sudo usermod -s /bin/zsh bahaa
+echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> /home/bahaa/.zshrc
+echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> /home/bahaa/.zshrc
+echo "source /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh" >> /home/bahaa/.zshrc
+
 
 sudo pacman -S --needed --noconfirm --disable-download-timeout go base-devel
 
