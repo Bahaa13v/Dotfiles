f6ca031401a1ea40947d33df1dbe60342e2f6450
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-08 08:38:09 +0100

    Update Dotfiles.sh

--- Dotfiles.sh
+++ Dotfiles.sh
@@ -18,6 +18,8 @@ cp -r walls /home/bahaa/
 
 sudo pacman -S --needed --noconfirm --disable-download-timeout bash-preexec grml-zsh-config powerline python-click-completion zsh zsh-autosuggestions zsh-completions zsh-doc zsh-history-substring-search zsh-lovers zsh-syntax-highlighting zshdb
 
+sudo usermod -s /bin/zsh bahaa
+
 sudo pacman -S --needed --noconfirm --disable-download-timeout go base-devel
 
 
