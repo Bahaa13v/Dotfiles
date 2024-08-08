453103d8bf4f8800399d703af4574046b76ef955
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-08 08:35:38 +0100

    Update Extra.sh

--- Extra.sh
+++ Extra.sh
@@ -2,10 +2,6 @@ echo "Basic Needes"
 
 sudo pacman -S --needed --noconfirm --disable-download-timeout ttf-dejavu ttf-joypixels pulseaudio pavucontrol dolphin
 
-echo "Zsh"
-
-sudo pacman -S --needed --noconfirm --disable-download-timeout bash-preexec grml-zsh-config powerline python-click-completion zsh zsh-autosuggestions zsh-completions zsh-doc zsh-history-substring-search zsh-lovers zsh-syntax-highlighting zshdb
-
 echo "Browser"
 
 sudo pacman -S --needed --noconfirm --disable-download-timeout chromium xorg xorg-server xorg-xinit xorg-drivers
