e2a9ea94c5aa5bdebc455a4a232e9eca7a8238c6
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-08 08:20:52 +0100

    Create Extra.sh

---
+++ Extra.sh
@@ -0,0 +1,15 @@
+echo "Basic Needes"
+
+sudo pacman -S --needed --noconfirm --disable-download-timeout ttf-dejavu ttf-joypixels pulseaudio pavucontrol dolphin
+
+echo "Zsh"
+
+sudo pacman -S --needed --noconfirm --disable-download-timeout bash-preexec grml-zsh-config powerline python-click-completion zsh zsh-autosuggestions zsh-completions zsh-doc zsh-history-substring-search zsh-lovers zsh-syntax-highlighting zshdb
+
+echo "Browser"
+
+sudo pacman -S --needed --noconfirm --disable-download-timeout chromium xorg xorg-server xorg-xinit xorg-drivers
+
+echo "Other.."
+
+sudo pacman -S --needed --noconfirm --disable-download-timeout ttf-sazanami unzip wget fastfetch htop linux-headers linux-docs meson ninja
