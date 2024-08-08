a5abbaa94d3396cd0c11aa2b84760cd0328d4cd4
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-08 13:09:43 +0100

    Create Dotfiles.sh

---
+++ Dotfiles.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+sudo pacman -S --needed --noconfirm --disable-download-timeout sddm sway
+sudo systemctl enable sddm
+sudo pacman -S --needed --noconfirm --disable-download-timeout swaybg waybar alacritty wofi neofetch awesome-terminal-fonts ttf-font-awesome ttf-hack-nerd
+
+mv packages.txt /home/bahaa/
+
+mkdir /home/bahaa/.config
+
+cp -r alacritty /home/bahaa/.config/
+cp -r sway /home/bahaa/.config/
+cp -r neofetch /home/bahaa/.config/
+cp -r waybar /home/bahaa/.config/
+cp -r wofi /home/bahaa/.config/
+
+cp -r walls /home/bahaa/
+
+sudo pacman -S --needed --noconfirm --disable-download-timeout bash-preexec grml-zsh-config powerline python-click-completion zsh zsh-autosuggestions zsh-completions zsh-doc zsh-history-substring-search zsh-lovers zsh-syntax-highlighting zshdb
+
+sudo usermod -s /bin/zsh bahaa
+
+sudo pacman -S --needed --noconfirm --disable-download-timeout go base-devel
+
+
+cd ..
+git clone https://aur.archlinux.org/yay
+cd yay
+makepkg -si --noconfirm
+cd ../Dotfiles
+
+
+yay -S --noconfirm --disable-download-timeout ttf-geist ttf-geist-mono
+
+read -p "Do you want to get the extra stuff? (y/n): " answer
+
+if [[ "$answer" == "y" ]]; then
+    chmod +x Extra.sh
+    ./Extra.sh
+else
+    echo "look out for what you need"
+    cat Extra.sh
+fi
