b14a5f5918ebc36f2903d407a023db42a9ba6743
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-10 21:05:48 +0100

    Update Dotfiles.sh

--- Dotfiles.sh
+++ Dotfiles.sh
@@ -4,7 +4,7 @@ sudo pacman -S --needed --noconfirm --disable-download-timeout sddm sway
 sudo systemctl enable sddm
 sudo pacman -S --needed --noconfirm --disable-download-timeout swaybg waybar alacritty wofi neofetch awesome-terminal-fonts ttf-font-awesome ttf-hack-nerd
 
-mkdir /home/bahaa/.config
+mkdir -p /home/bahaa/.config
 
 cp -r alacritty /home/bahaa/.config/
 cp -r sway /home/bahaa/.config/
@@ -16,13 +16,13 @@ cp -r walls /home/bahaa/
 
 
 echo "Setting up Zsh..."
+sleep 3
 sudo pacman -S --needed --noconfirm --disable-download-timeout bash-preexec grml-zsh-config powerline python-click-completion zsh zsh-autosuggestions zsh-completions zsh-doc zsh-history-substring-search zsh-lovers zsh-syntax-highlighting zshdb
 
 sudo usermod -s /bin/zsh bahaa
 echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> /home/bahaa/.zshrc
 echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> /home/bahaa/.zshrc
 echo "source /usr/share/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh" >> /home/bahaa/.zshrc
-
 
 sudo pacman -S --needed --noconfirm --disable-download-timeout go base-devel
 
