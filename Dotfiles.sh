d4307c1ded150f75bcea0b44e5750fa9b956b12d
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-12 22:11:59 +0100

    Update Dotfiles.sh

--- Dotfiles.sh
+++ Dotfiles.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
-
-sudo pacman -S --needed --noconfirm --disable-download-timeout sddm sway
-sudo systemctl enable sddm
-sudo pacman -S --needed --noconfirm --disable-download-timeout swaybg waybar alacritty wofi neofetch awesome-terminal-fonts ttf-font-awesome ttf-hack-nerd
-
+sudo pacman -S --needed --noconfirm --disable-download-timeout sway sddm swaybg waybar alacritty wofi neofetch awesome-terminal-fonts ttf-font-awesome ttf-hack-nerd
 mkdir -p /home/bahaa/.config
-
 cp -r alacritty /home/bahaa/.config/
 cp -r sway /home/bahaa/.config/
 cp -r neofetch /home/bahaa/.config/
 cp -r waybar /home/bahaa/.config/
 cp -r wofi /home/bahaa/.config/
-
 cp -r walls /home/bahaa/
-
-
 echo "Setting up Zsh..."
 sleep 3
 sudo pacman -S --needed --noconfirm --disable-download-timeout bash-preexec grml-zsh-config powerline python-click-completion zsh zsh-autosuggestions zsh-completions zsh-doc zsh-history-substring-search zsh-lovers zsh-syntax-highlighting zshdb
-
 sudo usermod -s /bin/zsh bahaa
 echo "source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> /home/bahaa/.zshrc
 echo "source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> /home/bahaa/.zshrc
