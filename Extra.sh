c86a46d16c4a461ec0581334296fff176b754350
Bahaa <121813996+Bahaa13v@users.noreply.github.com>
2024-08-21 21:35:19 +0100

    Update Extra.sh

--- Extra.sh
+++ Extra.sh
@@ -1,13 +1,13 @@
 echo "Basic Needes..."
 sleep 3
-sudo pacman -S --needed --noconfirm --disable-download-timeout ttf-dejavu ttf-joypixels pulseaudio pavucontrol dolphin
+sudo pacman -S --noconfirm --disable-download-timeout ttf-dejavu ttf-dejavu-nerd ttf-joypixels pulseaudio pavucontrol
 
 echo "Browser..."
 sleep 3
-sudo pacman -S --needed --noconfirm --disable-download-timeout chromium xorg xorg-server xorg-xinit xorg-drivers
+yay -S --noconfirm --disable-download-timeout google-chrome
 
 echo "Other..."
 sleep 3
-sudo pacman -S --needed --noconfirm --disable-download-timeout ttf-sazanami zip unzip wget fastfetch htop linux-headers linux-docs meson ninja
+sudo pacman -S --needed --noconfirm --disable-download-timeout ttf-hanazono zip unzip wget fastfetch htop linux-headers linux-docs meson ninja
 
 echo "Done."
